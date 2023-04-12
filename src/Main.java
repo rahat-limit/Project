6d5f64d316634268b277a8abca864d213fe14a02
rakhat_cyanid <zhayshilik@gmail.com>
2023-04-12 16:40:00 +0600

    How start, Documentation

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,22 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-//    MyArrayList list = new MyArrayList<Integer>();
+//    MyArrayList list = new MyArrayList();
 //    int num = scan.nextInt();
 //    Firstly, you can add to List any type of Objects. Then, use each of various methods.
 //    list.add(1);
 //    System.out.println(list.get(0));
+//    -------- Options:
+//    list.get(index);
+//    list.size();
+//    list.contains(Object);
+//    list.add(Object);
+//    list.addAt(Object, index);
+//    list.remove(Object);
+//    list.remove(index);
+//    list.clear();
+//    list.indexOf(Object);
+//    list.lastIndexOf(Object);
+//    list.sort();
   }
 }
