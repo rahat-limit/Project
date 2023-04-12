6c73ad39e8921880f5e1349b7e1afd541bff4013
rakhat_cyanid <zhayshilik@gmail.com>
2023-04-12 16:48:43 +0600

    How to start, Documentation

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
 //    Firstly, you can add to List any type of Objects. Then, use each of various methods.
 //    -------- Example:
 //    MyArrayList list = new MyArrayList();
-//    System.out.println("We already started, just enter input data: (For example:(Integer)). ");
-//    int num = scan.nextInt();
-//    list.add(1);
+//    System.out.println("We already started, just enter input data: (For example:(Integer)). Also it works the same way to any type of Object.");
+//    int input = scan.nextInt(); or String input = scan.nextLine();
+//    list.add(input);
 //    System.out.println(list.get(0));
 
 //    -------- Options:
