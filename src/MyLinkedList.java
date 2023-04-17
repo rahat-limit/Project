24035b81f3d74c0afeabf99e2e993cfb73101063
rakhat_cyanid <zhayshilik@gmail.com>
2023-04-17 12:36:35 +0600

    MyLinkedList init

---
+++ src/MyLinkedList.java
@@ -0,0 +1,56 @@
+public class MyLinkedList implements MyList{
+  @Override
+  public int size() {
+    return 0;
+  }
+
+  @Override
+  public boolean contains(Object o) {
+    return false;
+  }
+
+  @Override
+  public void add(Object item) {
+
+  }
+
+  @Override
+  public void addAt(Object item, int index) {
+
+  }
+
+  @Override
+  public boolean remove(Object item) {
+    return false;
+  }
+
+  @Override
+  public Object remove(int index) {
+    return null;
+  }
+
+  @Override
+  public void clear() {
+
+  }
+
+  @Override
+  public Object get(int index) {
+    return null;
+  }
+
+  @Override
+  public int indexOf(Object o) {
+    return 0;
+  }
+
+  @Override
+  public int lastIndexOf(Object o) {
+    return 0;
+  }
+
+  @Override
+  public void sort() {
+
+  }
+}
