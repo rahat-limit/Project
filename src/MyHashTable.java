65b75c5e2aabdbd5996b7ae177c33f0b5072ff26
rakhat_cyanid <zhayshilik@gmail.com>
2023-05-14 11:51:16 +0600

    My Hash Table init

---
+++ src/MyHashTable.java
@@ -0,0 +1,26 @@
+public class MyHashTable<K, V> {
+  private int M = 11: // default number of chains
+  private int size;
+  public MyHashTable () {
+
+  }
+  public MyHashTable(int M) {}
+  private int hash (K key) {
+    return 0;
+  }
+  public void put (K key, V wvalue) {
+
+  }
+  public V get (K key) {
+    return;
+  }
+  public V remove(K key) {
+    return;
+  }
+  public boolean contains(V value) {
+    return true;
+  }
+  public K getKey(V value) {
+    return;
+  }
+}
